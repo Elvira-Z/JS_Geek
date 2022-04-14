0ad4f0d16d887f7e85df445bbd37448c4d12c278
Elvira-Z <95440648+Elvira-Z@users.noreply.github.com>
2022-04-14 15:53:32 +0300

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,42 @@
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ],
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg",
+        ],
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+//Необходимо вывести в консоль массив продуктов 
+//в котором есть хоть одна фотография используя метод filter.
+// Исходные данные - массив products.
+
+console.log(products.filter(objects => "photos" in objects && objects.photos.length !== 0));
+
+
+//Необходимо отсортировать массив products используя метод 
+//sort по цене, начиная с самой маленькой, заканчивая самой большой ценой, 
+//после чего вывести отсортированный массив в консоль.
+
+products.sort(function (a, b) {
+    return a.price - b.price
+})
+console.log(products);
